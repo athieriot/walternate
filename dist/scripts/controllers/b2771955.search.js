9402b59a09f87caecfff84cc53350bd46e120036
Aurélien Thieriot <a.thieriot@gmail.com>
2013-02-18 22:44:33 +0000

    Update

---
+++ dist/scripts/controllers/b2771955.search.js
@@ -0,0 +1,19 @@
+'use strict';
+
+walternateApp.controller('SearchController', ['$scope', '$resource', function($scope, $resource) {
+   $scope.configuration = $resource("/api/configuration", {format:'json'} ).get();
+
+   $scope.moviedb = $resource("/api/searchMovie/:title", {title:'@title', format:'json'});
+
+   $scope.search = function(movieTitle) {
+      $scope.movies = $scope.moviedb.get({title: movieTitle});
+   }
+
+   $scope.popular = function(movie) {
+      return movie.vote_count > 10;
+   }
+
+   $scope.clear = function() {
+      $scope.movies = {};
+   }
+}]);
